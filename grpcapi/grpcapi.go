2424cdc129842f4b3fcf0593664ded59f88154ed
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-17 10:22:51 +0600

    pb to mdb added

--- grpcapi/grpcapi.go
+++ grpcapi/grpcapi.go
@@ -1,4 +1,27 @@
 package grpcapi
 
+import (
+	"database/sql"
+	"mailinglist/mdb"
+	"time"
+)
+
 type MailServer struct {
+	pb.UnimplementedMailingListServiceServer
+	db *sql.DB
+}
+
+func pbEntryToMdbEntry(pbEntry *pb.EmailEntry) mdb.EmailEntry {
+
+	t := time.Unix(pbEntry.ConfirmedAt, 0)
+	return mdb.EmailEntry{
+		Id:        pbEntry.Id,
+		Email:     pbEntry.Email,
+		ConfirmAt: &t,
+		OptOut:    pbEntry.OptOut,
+	}
+}
+
+func mdbEntryToPbEntry(mdbEntry *mdb.EmailEntry) pb.EmailEntry {
+
 }
