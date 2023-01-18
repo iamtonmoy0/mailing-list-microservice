1ae15986a0ba1ea2593ae4736e27fd2986a29abf
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-18 15:27:14 +0600

    error fixed

--- grpcapi/grpcapi.go
+++ grpcapi/grpcapi.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"log"
 	"mailinglist/mdb"
+	pb "mailinglist/proto"
 	"net"
 	"time"
 
@@ -17,13 +18,12 @@ type MailServer struct {
 }
 
 func pbEntryToMdbEntry(pbEntry *pb.EmailEntry) mdb.EmailEntry {
-
 	t := time.Unix(pbEntry.ConfirmedAt, 0)
 	return mdb.EmailEntry{
-		Id:        pbEntry.Id,
-		Email:     pbEntry.Email,
-		ConfirmAt: &t,
-		OptOut:    pbEntry.OptOut,
+		Id:          pbEntry.Id,
+		Email:       pbEntry.Email,
+		ConfirmedAt: &t,
+		OptOut:      pbEntry.OptOut,
 	}
 }
 
@@ -31,13 +31,12 @@ func mdbEntryToPbEntry(mdbEntry *mdb.EmailEntry) pb.EmailEntry {
 	return pb.EmailEntry{
 		Id:          mdbEntry.Id,
 		Email:       mdbEntry.Email,
-		ConfirmedAt: mdbEntry.ConfirmAt.Unix(),
+		ConfirmedAt: mdbEntry.ConfirmedAt.Unix(),
 		OptOut:      mdbEntry.OptOut,
 	}
 }
 
 func emailResponse(db *sql.DB, email string) (*pb.EmailResponse, error) {
-
 	entry, err := mdb.GetEmail(db, email)
 	if err != nil {
 		return &pb.EmailResponse{}, err
@@ -45,38 +44,41 @@ func emailResponse(db *sql.DB, email string) (*pb.EmailResponse, error) {
 	if entry == nil {
 		return &pb.EmailResponse{}, nil
 	}
+
 	res := mdbEntryToPbEntry(entry)
 
 	return &pb.EmailResponse{EmailEntry: &res}, nil
-
 }
-func (s *MailServer) GetEmail(ctx context.Context, req *pb.GetEmailRequest) (*pb.EmailResponse, error) {
 
-	log.Printf("grpc GetEmail:%v\n", req)
+func (s *MailServer) GetEmail(ctx context.Context, req *pb.GetEmailRequest) (*pb.EmailResponse, error) {
+	log.Printf("gRPC GetEmail: %v\n", req)
 	return emailResponse(s.db, req.EmailAddr)
 }
 
-func (s *MailServer) GetEmailBatch(ctx context.Context, req *pb.GetEmailBatch) (*pb.GetEmailBatchResponse, error) {
-	log.Printf("grpc GetEmailBatch:%v\n", req)
+func (s *MailServer) GetEmailBatch(ctx context.Context, req *pb.GetEmailBatchRequest) (*pb.GetEmailBatchResponse, error) {
+	log.Printf("gRPC GetEmailBatch: page:%v count:%v", req.Page, req.Count)
+
 	params := mdb.GetEmailBatchQueryParams{
 		Page:  int(req.Page),
 		Count: int(req.Count),
 	}
+
 	mdbEntries, err := mdb.GetEmailBatch(s.db, params)
 	if err != nil {
 		return &pb.GetEmailBatchResponse{}, err
 	}
+
 	pbEntries := make([]*pb.EmailEntry, 0, len(mdbEntries))
 	for i := 0; i < len(mdbEntries); i++ {
 		entry := mdbEntryToPbEntry(&mdbEntries[i])
 		pbEntries = append(pbEntries, &entry)
-
 	}
+
 	return &pb.GetEmailBatchResponse{EmailEntries: pbEntries}, nil
 }
-func (s *MailServer) CreateEmail(ctx context.Context, req *pb.CreateEmailRequest) (*pb.EmailResponse, error) {
 
-	log.Printf("grpc CreateEmail:%v\n", req)
+func (s *MailServer) CreateEmail(ctx context.Context, req *pb.CreateEmailRequest) (*pb.EmailResponse, error) {
+	log.Printf("gRPC CreateEmail: %v\n", req)
 
 	err := mdb.CreateEmail(s.db, req.EmailAddr)
 	if err != nil {
@@ -85,21 +87,22 @@ func (s *MailServer) CreateEmail(ctx context.Context, req *pb.CreateEmailRequest
 
 	return emailResponse(s.db, req.EmailAddr)
 }
-func (s *MailServer) UpdateEmail(ctx context.Context, req *pb.UpdateEmailRequest) (*pb.EmailResponse, error) {
 
-	log.Printf("grpc UpdateEmail:%v\n", req)
+func (s *MailServer) UpdateEmail(ctx context.Context, req *pb.UpdateEmailRequest) (*pb.EmailResponse, error) {
+	log.Printf("gRPC UpdateEmail: %v\n", req)
+
 	entry := pbEntryToMdbEntry(req.EmailEntry)
 
-	err := mdb.UpdateEmail(s.db, req.EmailAddr)
+	err := mdb.UpdateEmail(s.db, entry)
 	if err != nil {
 		return &pb.EmailResponse{}, err
 	}
 
 	return emailResponse(s.db, entry.Email)
 }
-func (s *MailServer) DeleteEmail(ctx context.Context, req *pb.DeleteEmailRequest) (*pb.EmailResponse, error) {
 
-	log.Printf("grpc DeleteEmail:%v\n", req)
+func (s *MailServer) DeleteEmail(ctx context.Context, req *pb.DeleteEmailRequest) (*pb.EmailResponse, error) {
+	log.Printf("gRPC DeleteEmail: %v\n", req)
 
 	err := mdb.DeleteEmail(s.db, req.EmailAddr)
 	if err != nil {
@@ -110,18 +113,19 @@ func (s *MailServer) DeleteEmail(ctx context.Context, req *pb.DeleteEmailRequest
 }
 
 func Serve(db *sql.DB, bind string) {
-
 	listener, err := net.Listen("tcp", bind)
 	if err != nil {
-		log.Fatalf("grpc server error:failure to bind %v\n", bind)
-
+		log.Fatal("gRPC server error: failure to bind %v\n", bind)
 	}
-	grpcServer := grpc.NewServer()
-	mailServer := MailServer{db: db}
-	pb.RegisterMailingListServiceServer(grpcServer, &mailServer)
-	log.Printf("grpc api server listening on %v\n", bind)
-	if err := grpcServer.Serve(listener); err != nil {
-		log.Fatalf("grpc server error: %v\n", err)
 
+	grpcServer := grpc.NewServer()
+
+	mailServer := MailServer{db: db}
+
+	pb.RegisterMailingListServiceServer(grpcServer, &mailServer)
+
+	log.Printf("gRPC API server listening on %v\n", bind)
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatal("gRPC server error: %v\n", err)
 	}
 }
