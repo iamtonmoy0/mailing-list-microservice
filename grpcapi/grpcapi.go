20e8e795c5596ece925e82de4d8593fcc2e3c8f7
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-18 09:11:16 +0600

    email batch and create mail added

--- grpcapi/grpcapi.go
+++ grpcapi/grpcapi.go
@@ -52,3 +52,31 @@ func (s *MailServer) GetEmail(ctx context.Context, req *pb.GetEmailRequest) (*pb
 	log.Printf("grpc GetEmail:%v\n", req)
 	return emailResponse(s.db, req.EmailAddr)
 }
+
+func (s *MailServer) GetEmailBatch(ctx context.Context, req *pb.GetEmailBatch) (*pb.GetEmailBatchResponse, error) {
+	log.Printf("grpc GetEmailBatch:%v\n", req)
+	params := mdb.GetEmailBatchQueryParams{
+		Page:  int(req.Page),
+		Count: int(req.Count),
+	}
+	mdbEntries, err := mdb.GetEmailBatch(s.db, params)
+	if err != nil {
+		return &pb.GetEmailBatchResponse{}, err
+	}
+	pbEntries := make([]*pb.EmailEntry, 0, len(mdbEntries))
+	for i := 0; i < len(mdbEntries); i++ {
+		entry := mdbEntryToPbEntry(&mdbEntries[i])
+		pbEntries = append(pbEntries, &entry)
+
+	}
+}
+func (s *MailServer) CreateEmail(ctx context.Context, req *pb.CreateEmailRequest) (*pb.EmailResponse, error) {
+
+	log.Printf("grpc GetEmail:%v\n", req)
+
+	err := mdb.CreateEmail(s.db, req.EmailAddr)
+	if err != nil {
+		return &pb.EmailResponse{}, err
+	}
+	return emailResponse(s.db, req.EmailAddr)
+}
